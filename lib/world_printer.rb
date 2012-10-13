3f6d5e8be05d11f2b0ab572a26f008099919519e
Lucas Parry <lucas@envato.com>
2012-10-13 21:35:35 +1100

    world printer

---
+++ lib/world_printer.rb
@@ -0,0 +1,32 @@
+require File.expand_path("../world", __FILE__)
+class WorldPrinter
+  def initialize(start_x, start_y, width, height)
+    @start_x, @start_y, @width, @height = start_x, start_y, width, height
+  end
+
+  def row_range
+    (@start_y...(@start_y + @height))
+  end
+
+  def column_range
+    (@start_x...(@start_x + @width))
+  end
+
+  def render(world)
+    row_range.map{|row| render_row(world, row) }
+  end
+
+  def render_row(world, y)
+    column_range.map do |x|
+      world.cells.include?(Point(x, y))
+    end
+  end
+
+  def print(world)
+    render(world).map do |line|
+      line.map do |char|
+        char ? "x" : " "
+      end.join
+    end.join("\n")
+  end
+end
---
+++ spec/world_printer_spec.rb
@@ -0,0 +1,35 @@
+require File.expand_path("../../lib/world_printer", __FILE__)
+
+describe WorldPrinter do
+  let(:start_x) { 0 }
+  let(:start_y) { 0 }
+  let(:width)   { 4 }
+  let(:height)  { 5 }
+  let(:world)   { World.new([Cell.new(1,2), Cell.new(1,3), Cell.new(1,4)]) }
+  subject{ WorldPrinter.new(start_x, start_y, width, height) }
+
+  it "should calculate a row" do
+    subject.render_row(world, 3).should == [false, true, false, false]
+  end
+
+  it "should make a pretty string" do
+    subject.print(world).should == [
+      "    ",
+      "    ",
+      " x  ",
+      " x  ",
+      " x  ",
+    ].join("\n")
+  end
+
+  it "should calculate the world" do
+    subject.render(world).should == [
+      [false, false, false, false],
+      [false, false, false, false],
+      [false, true, false, false],
+      [false, true, false, false],
+      [false, true, false, false]
+    ]
+  end
+
+end
